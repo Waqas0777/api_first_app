b8691175069f44cfa6e193c247a11ae5aaf4b0e9
waqas <waqas@knowpakistan.org>
2023-02-15 18:53:40 +0500

    Fetched Data from Apis and show its result in a listview.builder in separated cards . and when we click on a specific card we show its result in a details screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import 'package:api_first_app/users/cubit/user_cubit.dart';
-import 'package:api_first_app/users/user_screen.dart';
-=======
 import 'package:api_first_app/screens/posts/cubit/post_cubit.dart';
 import 'package:api_first_app/screens/posts/post_screen.dart';
->>>>>>> e1da34e (Fetched Data from Apis and show its result in a listview.builder in separated cards . and when we click on a specific card we show its result in a details screen.)
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -20,11 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-<<<<<<< HEAD
         BlocProvider<UserCubit>(create: (BuildContext context) => UserCubit()),
-=======
         BlocProvider<PostCubit>(create: (BuildContext context) => PostCubit()),
->>>>>>> e1da34e (Fetched Data from Apis and show its result in a listview.builder in separated cards . and when we click on a specific card we show its result in a details screen.)
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -32,11 +25,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-<<<<<<< HEAD
-        home: const UserScreen(),
-=======
         home: const PostScreen(),
->>>>>>> e1da34e (Fetched Data from Apis and show its result in a listview.builder in separated cards . and when we click on a specific card we show its result in a details screen.)
       ),
     );
   }
